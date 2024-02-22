0b4f184d8dd09a65283555b6a625a36ed4e3ffe1
hunjixin <1084400399@qq.com>
2024-02-22 14:39:37 +0800

    feat: add storage config

--- src/lib/components/repoCreateForm.tsx
+++ src/lib/components/repoCreateForm.tsx
@@ -6,9 +6,11 @@ import Form from "react-bootstrap/Form";
 export const RepositoryCreateForm = ( {id, onSubmit,setFormValid} ) => {
     const [repoName, setRepoName] = useState('');
     const [description, setDescription] = useState('');
+    const [storageCfg, setStorageCfg] = useState('');
   
     const repoNameField = useRef<HTMLInputElement>(null);
     const DescriptionField = useRef<HTMLInputElement>(null);
+    const storageCfgField = useRef<HTMLInputElement>(null);
   
     useEffect(() => {
       if (repoNameField.current) {
@@ -30,21 +32,28 @@ export const RepositoryCreateForm = ( {id, onSubmit,setFormValid} ) => {
       const handleDescriptionChange = (e) => {
         setDescription(e.target.value);
       };
-
+    
+      const handleStorageCfgChange = (e) => {
+        setStorageCfg(e.target.value);
+      };
     return (
         <Form id={id} onSubmit={(e) => {
             e.preventDefault();
             onSubmit({
                 name: repoNameField.current? repoNameField.current.value :'',
                 Description: DescriptionField.current ?DescriptionField.current.value : '',
+                blockstore_config: storageCfgField.current ?storageCfgField.current.value : '',
               });
         }}>
             <h4 className="mb-3">Create A New Repository</h4>
             <FloatingLabel label="Repository Name" controlId="repositryIdControl">
             <Form.Control type="text" ref={repoNameField}  placeholder="my-data-lake" onChange={handleRepoNameChange}/>               
             </FloatingLabel>
-            <FloatingLabel label="Description" controlId="repositryIdControl">
-            <Form.Control type="text" ref={DescriptionField}  placeholder="my-data-lake" onChange={handleDescriptionChange}/>               
+            <FloatingLabel label="Description" controlId="repositryDescControl">
+            <Form.Control type="text" ref={DescriptionField}  placeholder="my-data-lake" onChange={handleDescriptionChange}/>  
+            </FloatingLabel>
+            <FloatingLabel label="storage config" controlId="repositryStorageCfgControl">
+            <Form.Control type="text" ref={storageCfgField}  placeholder="my-data-lake" onChange={handleStorageCfgChange}/>               
             </FloatingLabel>
         </Form>
     );
