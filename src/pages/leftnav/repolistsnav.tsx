25f4106a6fbf8702a9621d6a4dc7c109479a5f28
guojiang <918941196@qq.com>
2024-08-15 18:58:30 +0800

    fix fil to filter

--- src/pages/leftnav/repolistsnav.tsx
+++ src/pages/leftnav/repolistsnav.tsx
@@ -1,42 +1,99 @@
-import {   Button, Card, Col, Form, Row } from "react-bootstrap"
+import { Button, Card, Col, Form, Row } from "react-bootstrap";
 import React, { useState } from "react";
 import { useRouter } from "../../lib/hooks/router";
 import { IoHomeOutline } from "react-icons/io5";
-import { GoRepo ,GoRepoForked,GoArchive,GoMirror,GoRepoTemplate } from "react-icons/go";
+import {
+  GoRepo,
+  GoRepoForked,
+  GoArchive,
+  GoMirror,
+  GoRepoTemplate,
+} from "react-icons/go";
 import { RiGitRepositoryPrivateLine } from "react-icons/ri";
 import { TbSourceCode } from "react-icons/tb";
 
+const Repolistsnav = () => {
+  const router = useRouter();
+  const [filter, setFil] = useState("");
 
-const Repolistsnav = () =>{
-    const router = useRouter()
-    const [fil, setFil] = useState('');
-    
-    const linkTo =(query:string)=>{
-        setFil(query)
-        router.push({ pathname: `/repositories/`, query: {fil:query} });
-    }
-    return(
-        <Row className="sidebar">
-        <Form className='flex Fast-navb'>
-            <Col className="d-flex">
-            <strong className="Navtittle">Repositories</strong>           
-            </Col>
+  const linkTo = (query: string) => {
+    setFil(query);
+    router.push({ pathname: `/repositories/`, query: { filter: query } });
+  };
+  return (
+    <Row className="sidebar">
+      <Form className="flex Fast-navb">
+        <Col className="d-flex">
+          <strong className="Navtittle">Repositories</strong>
+        </Col>
         <Card>
-            <Card.Body className="Nav-Card">
-                <Button onClick={()=>linkTo('') } className={fil==''?'active':''}><IoHomeOutline />All</Button>
-                <Button onClick={()=>linkTo('Public')} className={fil=='Public'?'active':''}><GoRepo />Public</Button>
-                <Button onClick={()=>linkTo('Private')} className={fil=='Private'?'active':''}><RiGitRepositoryPrivateLine />Private</Button>
-                <Button disabled onClick={()=>linkTo('Sources')} className={fil=='Sources'?'active':''}><TbSourceCode />Sources</Button>
-                <Button disabled onClick={()=>linkTo('Forks')} className={fil=='Forks'?'active':''}><GoRepoForked />Forks</Button>
-                <Button disabled onClick={()=>linkTo('Archived')} className={fil=='Archived'?'active':''}><GoArchive />Archived</Button>
-                <Button disabled onClick={()=>linkTo('Mirrors')} className={fil=='Mirrors'?'active':''}><GoMirror />Mirrors</Button>
-                <Button disabled onClick={()=>linkTo('Templates')} className={fil=='Templates'?'active':''}><GoRepoTemplate />Templates</Button>
-            </Card.Body>
+          <Card.Body className="Nav-Card">
+            <Button
+              onClick={() => linkTo("")}
+              className={filter == "" ? "active" : ""}
+            >
+              <IoHomeOutline />
+              All
+            </Button>
+            <Button
+              onClick={() => linkTo("Public")}
+              className={filter == "Public" ? "active" : ""}
+            >
+              <GoRepo />
+              Public
+            </Button>
+            <Button
+              onClick={() => linkTo("Private")}
+              className={filter == "Private" ? "active" : ""}
+            >
+              <RiGitRepositoryPrivateLine />
+              Private
+            </Button>
+            <Button
+              disabled
+              onClick={() => linkTo("Sources")}
+              className={filter == "Sources" ? "active" : ""}
+            >
+              <TbSourceCode />
+              Sources
+            </Button>
+            <Button
+              disabled
+              onClick={() => linkTo("Forks")}
+              className={filter == "Forks" ? "active" : ""}
+            >
+              <GoRepoForked />
+              Forks
+            </Button>
+            <Button
+              disabled
+              onClick={() => linkTo("Archived")}
+              className={filter == "Archived" ? "active" : ""}
+            >
+              <GoArchive />
+              Archived
+            </Button>
+            <Button
+              disabled
+              onClick={() => linkTo("Mirrors")}
+              className={filter == "Mirrors" ? "active" : ""}
+            >
+              <GoMirror />
+              Mirrors
+            </Button>
+            <Button
+              disabled
+              onClick={() => linkTo("Templates")}
+              className={filter == "Templates" ? "active" : ""}
+            >
+              <GoRepoTemplate />
+              Templates
+            </Button>
+          </Card.Body>
         </Card>
-        </Form>
-        </Row>
-    );
-  }
+      </Form>
+    </Row>
+  );
+};
 
-
-export default Repolistsnav
+export default Repolistsnav;
--- src/pages/repositories/index.tsx
+++ src/pages/repositories/index.tsx
@@ -1,8 +1,14 @@
-
 import React, { useCallback, useContext, useEffect, useState } from "react";
-import { ButtonToolbar, Card, Container, Dropdown, Form, FormControl } from "react-bootstrap";
+import {
+  ButtonToolbar,
+  Card,
+  Container,
+  Dropdown,
+  Form,
+  FormControl,
+} from "react-bootstrap";
 import { GoRepoPush } from "react-icons/go";
-import { HiSortAscending  } from "react-icons/hi";
+import { HiSortAscending } from "react-icons/hi";
 import { PiSortAscendingBold } from "react-icons/pi";
 
 import dayjs from "dayjs";
@@ -10,154 +16,201 @@ import relativeTime from "dayjs/plugin/relativeTime";
 
 import Layout from "../../lib/components/layout";
 import { ActionsBar } from "../../lib/components/controls";
-import { cache } from '../../lib/api';
+import { cache } from "../../lib/api";
 import { useRouter } from "../../lib/hooks/router";
 import { PiTextAaBold } from "react-icons/pi";
 
 import { Route, Routes } from "react-router-dom";
-import { CreateRepositoryButton, CreateRepositoryModal, RepositoryList } from "./repos-comp";
+import {
+  CreateRepositoryButton,
+  CreateRepositoryModal,
+  RepositoryList,
+} from "./repos-comp";
 import { users } from "../../lib/api/interface/index";
 import { ActivepageContext } from "../../lib/hooks/conf";
 import { activepage } from "../../lib/hooks/interface";
-type Order = 'asc' | 'desc';
-type SortBy = 'name' | 'created_at';
-const RepositoryPage = React.lazy(() => import('./repository'));
-
+type Order = "asc" | "desc";
+type SortBy = "name" | "created_at";
+const RepositoryPage = React.lazy(() => import("./repository"));
 
 dayjs.extend(relativeTime);
 
 const RepositoriesPage = () => {
-    const router = useRouter();
-    const [showCreateRepositoryModal, setShowCreateRepositoryModal] = useState(false);
-    const [sampleRepoChecked, setSampleRepoChecked] = useState(false);
-    const [createRepoError, setCreateRepoError] = useState(null);
-    const [refresh, setRefresh] = useState(false);
-    const [creatingRepo, setCreatingRepo] = useState(false);
-    const [repoamount, setRepoAmount] = useState(0);
-    const [search,setSearch] = useState('')
-    const [filter, setFilter] = useState('')
-    const [sortBy, setSortBy] = useState<SortBy>('name');
-    const [order, setOrder] = useState<Order>('asc');
-    function debounce(func: Function, delay: number) {
-        let timer: NodeJS.Timeout;
-        return  (...args: any[]) => {
-          clearTimeout(timer);
-          timer = setTimeout(() => {
-            func.apply(this, args);
-          }, delay);
-        };
-      }
-    const handleChange:React.ChangeEventHandler<HTMLInputElement> = (e)=>{
-        setSearch(e.target.value);
-    }
-    const debouncedHandleChange = debounce(handleChange, 300);
-
-    useEffect(() => {
-        if (!cache.get('token')) { router.push('/login') }
-    })
-    const activepageL: activepage = useContext(ActivepageContext)
-    useEffect(() => {
-        activepageL.setPage('repositories')
-    })
-    const createRepo = async (repo: { name: string, description: string ,visible:boolean,blockstore_config:string}, presentRepo = true) => {
-        const owner = cache.get('user')
-        try {
-            setCreatingRepo(true);
-            setCreateRepoError(null);
-            await users.createRepository(repo);
-            setRefresh(!refresh);
-            if (presentRepo) {
-                router.push({ pathname: `/repositories/:user/:repoId/objects`, params: { repoId: repo.name, user: owner }, query: {} });
-            }
-            return true;
-        } catch (error: any) {
-            setCreatingRepo(false);
-            setCreateRepoError(error);
-            return false;
-        }
+  const router = useRouter();
+  const [showCreateRepositoryModal, setShowCreateRepositoryModal] =
+    useState(false);
+  const [sampleRepoChecked, setSampleRepoChecked] = useState(false);
+  const [createRepoError, setCreateRepoError] = useState(null);
+  const [refresh, setRefresh] = useState(false);
+  const [creatingRepo, setCreatingRepo] = useState(false);
+  const [repoamount, setRepoAmount] = useState(0);
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("");
+  const [sortBy, setSortBy] = useState<SortBy>("name");
+  const [order, setOrder] = useState<Order>("asc");
+  function debounce(func: Function, delay: number) {
+    let timer: NodeJS.Timeout;
+    return (...args: any[]) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
     };
-    useEffect(()=>{
-        let fil = router.query.fil
-        setFilter(fil?fil:'')
-    },[router])
+  }
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearch(e.target.value);
+  };
+  const debouncedHandleChange = debounce(handleChange, 300);
 
-    const createRepositoryButtonCallback = useCallback(() => {
-        setSampleRepoChecked(false);
-        setShowCreateRepositoryModal(true);
-        setCreateRepoError(null);
-    }, [showCreateRepositoryModal, setShowCreateRepositoryModal]);
+  useEffect(() => {
+    if (!cache.get("token")) {
+      router.push("/login");
+    }
+  });
+  const activepageL: activepage = useContext(ActivepageContext);
+  useEffect(() => {
+    activepageL.setPage("repositories");
+  });
+  const createRepo = async (
+    repo: {
+      name: string;
+      description: string;
+      visible: boolean;
+      blockstore_config: string;
+    },
+    presentRepo = true
+  ) => {
+    const owner = cache.get("user");
+    try {
+      setCreatingRepo(true);
+      setCreateRepoError(null);
+      await users.createRepository(repo);
+      setRefresh(!refresh);
+      if (presentRepo) {
+        router.push({
+          pathname: `/repositories/:user/:repoId/objects`,
+          params: { repoId: repo.name, user: owner },
+          query: {},
+        });
+      }
+      return true;
+    } catch (error: any) {
+      setCreatingRepo(false);
+      setCreateRepoError(error);
+      return false;
+    }
+  };
+  useEffect(() => {
+    let filter = router.query.filter;
+    setFilter(filter ? filter : "");
+  }, [router]);
 
-    return (
-        <Layout>
-            <Container fluid="xl" className="mt-3">
-                {<ActionsBar>
-                    <h2 className="repoTittle"><strong>{filter?filter:'All'}</strong></h2>
-                    <ButtonToolbar className="ms-auto mb-2">
-                        <CreateRepositoryButton variant="primary" enabled={true} onClick={createRepositoryButtonCallback} />
-                    </ButtonToolbar>
-                </ActionsBar>}
-                <Form>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={debouncedHandleChange}/>
-                </Form>
+  const createRepositoryButtonCallback = useCallback(() => {
+    setSampleRepoChecked(false);
+    setShowCreateRepositoryModal(true);
+    setCreateRepoError(null);
+  }, [showCreateRepositoryModal, setShowCreateRepositoryModal]);
 
-                <Card className="repo-card">
-                    <Card.Header className="repo-card-header">
-                        <strong>{repoamount} repositories</strong>
-                        <Dropdown>
-        <Dropdown.Toggle variant="outline-primary" className="sortType"  id="dropdown-basic">
-          {order=='asc'?<HiSortAscending />:<PiSortAscendingBold />}
-          {sortBy=='name'?'Name':'Last Pushed'}
-        </Dropdown.Toggle>
-        <Dropdown.Menu className="sortMenu">
-          <Dropdown.Item onClick={() => setSortBy('name')}><PiTextAaBold />Name</Dropdown.Item>
-          <Dropdown.Item onClick={() => setSortBy('created_at')} className="UpsplitDropdown"><GoRepoPush />Last Pushed</Dropdown.Item>
-          <Dropdown.Item onClick={() => setOrder('asc')} className="DownsplitDropdown"><HiSortAscending />Ascending</Dropdown.Item>
-          <Dropdown.Item onClick={() => setOrder('desc')}><PiSortAscendingBold />Descending</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+  return (
+    <Layout>
+      <Container fluid="xl" className="mt-3">
+        {
+          <ActionsBar>
+            <h2 className="repoTittle">
+              <strong>{filter ? filter : "All"}</strong>
+            </h2>
+            <ButtonToolbar className="ms-auto mb-2">
+              <CreateRepositoryButton
+                variant="primary"
+                enabled={true}
+                onClick={createRepositoryButtonCallback}
+              />
+            </ButtonToolbar>
+          </ActionsBar>
+        }
+        <Form>
+          <FormControl
+            type="text"
+            placeholder="Search"
+            className="mr-sm-2"
+            onChange={debouncedHandleChange}
+          />
+        </Form>
 
-                    </Card.Header>
-                <RepositoryList
-                    refresh={refresh}
-                    setRepoAmount={setRepoAmount}
-                    search={search}
-                    filter={filter}
-                    sortBy={sortBy}
-                    order={order}
-                />
-                </Card>
+        <Card className="repo-card">
+          <Card.Header className="repo-card-header">
+            <strong>{repoamount} repositories</strong>
+            <Dropdown>
+              <Dropdown.Toggle
+                variant="outline-primary"
+                className="sortType"
+                id="dropdown-basic"
+              >
+                {order == "asc" ? <HiSortAscending /> : <PiSortAscendingBold />}
+                {sortBy == "name" ? "Name" : "Last Pushed"}
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="sortMenu">
+                <Dropdown.Item onClick={() => setSortBy("name")}>
+                  <PiTextAaBold />
+                  Name
+                </Dropdown.Item>
+                <Dropdown.Item
+                  onClick={() => setSortBy("created_at")}
+                  className="UpsplitDropdown"
+                >
+                  <GoRepoPush />
+                  Last Pushed
+                </Dropdown.Item>
+                <Dropdown.Item
+                  onClick={() => setOrder("asc")}
+                  className="DownsplitDropdown"
+                >
+                  <HiSortAscending />
+                  Ascending
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => setOrder("desc")}>
+                  <PiSortAscendingBold />
+                  Descending
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Card.Header>
+          <RepositoryList
+            refresh={refresh}
+            setRepoAmount={setRepoAmount}
+            search={search}
+            filter={filter}
+            sortBy={sortBy}
+            order={order}
+          />
+        </Card>
 
-
-                <CreateRepositoryModal
-                    onCancel={() => {
-                        setShowCreateRepositoryModal(false);
-                        setCreateRepoError(null);
-                    }}
-                    show={showCreateRepositoryModal}
-                    setShow={setShowCreateRepositoryModal}
-                    error={createRepoError}
-                    setRefresh={setRefresh}
-                    onSubmit={(repo) => createRepo(repo, true)}
-                    samlpleRepoChecked={sampleRepoChecked}
-                    inProgress={creatingRepo}
-                    refresh={refresh}
-                />
-
-            </Container>
-        </Layout>
-
-    );
-}
+        <CreateRepositoryModal
+          onCancel={() => {
+            setShowCreateRepositoryModal(false);
+            setCreateRepoError(null);
+          }}
+          show={showCreateRepositoryModal}
+          setShow={setShowCreateRepositoryModal}
+          error={createRepoError}
+          setRefresh={setRefresh}
+          onSubmit={(repo) => createRepo(repo, true)}
+          samlpleRepoChecked={sampleRepoChecked}
+          inProgress={creatingRepo}
+          refresh={refresh}
+        />
+      </Container>
+    </Layout>
+  );
+};
 
 const RepositoriesIndex = () => {
-
-    return (
-        <Routes>
-            <Route path="/" element={<RepositoriesPage />} />
-            <Route path=":user/:repoId/*" element={<RepositoryPage />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<RepositoriesPage />} />
+      <Route path=":user/:repoId/*" element={<RepositoryPage />} />
+    </Routes>
+  );
 };
 
 export default RepositoriesIndex;
-
