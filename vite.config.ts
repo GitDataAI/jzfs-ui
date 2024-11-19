b544c08f994bfc465d956d185cbd4a84c85d3ae5
preface <3476561861@qq.com>
2024-11-19 16:53:42 +0800

    :fire: delete test and update backend url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:34513",
+        target: "http://221.128.225.26:34513",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
