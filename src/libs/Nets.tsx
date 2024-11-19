07e8916ad70a47deed430097745982ccefdc1b38
ZhenYi <434836402@qq.com>
2024-11-20 06:56:49 +0800

    :wrench: igr ssl

--- src/libs/Nets.tsx
+++ src/libs/Nets.tsx
@@ -1,5 +1,6 @@
 import Urls from "./Urls.tsx";
 import axios from "axios";
+import * as https from "node:https";
 
 export interface Result<T> {
   data: {
@@ -13,7 +14,7 @@ class Nets extends Urls {
   constructor() {
     super();
   }
-  public baseUrl = "http://api.rspanel.cn";
+  public baseUrl = "https://api.rspanel.cn";
   // @eslint-disable-next-line @typescript-eslint/no-explicit-any
   public async POST(
     urls: string,
@@ -21,10 +22,14 @@ class Nets extends Urls {
     options: object = {}
   ): Promise<any> {
     try {
+      const agent = new https.Agent({
+        rejectUnauthorized: false
+      });
       return await axios.post(this.baseUrl + urls, data, {
         headers: {
           ...options,
         },
+        httpsAgent: agent
       });
     } catch (e) {
       return e;
