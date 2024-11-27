7795680fae73d0df878c97856ef6d6d2b86737f9
preface <3476561861@qq.com>
2024-11-27 12:23:38 +0800

    :lipstick:Add three retrieval process pages and modify the style

--- src/app/auth/SearchAccount.tsx
+++ src/app/auth/SearchAccount.tsx
@@ -4,7 +4,7 @@ import {Auth_api} from "@/store/useUsers.tsx";
 import Login from "@/app/auth/Login.tsx";
 import {useTranslation} from "react-i18next";
 import {Toast} from "primereact/toast";
-import Forgot from "@/app/auth/Forgot.tsx";
+import login from "@/app/auth/Login.tsx";
 
 const SearchAccount = () => {
     const [t] = useTranslation("Auth")
@@ -25,7 +25,7 @@ const SearchAccount = () => {
             //     nav(`/auth/Forgot?email=${res.data.email}`)
             // } else {
             //     // 邮箱不存在，导航到另一个页面
-            //     nav("/auth/no-email");
+            //     nav("/auth/FindAccount");
             // }
         } catch (error) {
             toast.current?.show({ severity: 'error', summary: t("Error"), detail: t("SomethingWrong") });
@@ -34,35 +34,39 @@ const SearchAccount = () => {
     };
 
 
-    const Forgot = () => {
-        nav("/auth/FindAccount")
+    const login = () => {
+        nav("/auth/login")
     }
     return (
         <>
             <Toast ref={toast}/>
             <div className="h-full">
                 <div className="flex items-baseline justify-center cursor-pointer">
-                    <img src="/gitdata.ai-black-redpanda.png" alt="JZFS" className=" w-52" />
+                    <img src="/gitdata.ai-black-redpanda.png" alt="JZFS" className=" w-52"/>
                     {/*<b className=" text-4xl">GitDataAI</b>*/}
                 </div>
-                <h3 className="text-center mt-6 mb-4 font-bold cursor-pointer">{t("ResetPass")}</h3>
+                <h3 className="text-center mt-6 mb-4 font-bold cursor-pointer">{t("FindAccount")}</h3>
                 <div>
                     <form className="flex flex-col items-center ">
-                        <input onChange={(e)=>Setuser((user)=>({...user,email:e.target.value}))} value={user.email} type="email" placeholder={t("Enter")+t("Email")} className="border border-[#8790a2] h-10 w-4/5 mt-2 mb-2 px-2 showDiv"/>
+                        <input onChange={(e) => Setuser((user) => ({...user, email: e.target.value}))}
+                               value={user.email} type="email" placeholder={t("Enter") + t("Email")}
+                               className="border border-[#8790a2] h-10 w-4/5 mt-2 mb-2 px-2"/>
 
-                        <button type={"button"} onClick={ checkEmailExist} className="bg-[#f34d01e6] border border-[#8790a2] h-10 w-4/5"><span className="text-white">{t("Search")}</span></button>
+                        <button type={"button"} onClick={checkEmailExist}
+                                className="bg-[#f34d01e6] border border-[#8790a2] h-10 w-4/5"><span
+                            className="text-white">{t("Search")}</span></button>
 
                     </form>
                 </div>
                 <div className="text-center mt-4 mb-5 cursor-pointer">
-                    <span className="mr-1 hover:text-[#f34d01e6]"><a onClick={Forgot}>{t("RemAccount")}</a></span>
+                    <span className="mr-1 hover:text-[#f34d01e6]"><a onClick={login}>{t("RemAccount")}</a></span>
                 </div>
                 <hr className="border-none h-px ml-auto mr-auto bg-[#c2c7d0] w-4/5"/>
                 <div className="flex items-baseline justify-center mt-1 cursor-pointer">
-                    <img src="/gitdata.ai-black-redpanda.png" alt="JZFS" className=" w-32" />
+                    <img src="/gitdata.ai-black-redpanda.png" alt="JZFS" className=" w-32"/>
                 </div>
                 <h3 className="text-center text-xs mb-2 cursor-pointer">{t("Connectivity")}</h3>
-                <h3 className="text-center text-xs cursor-pointer">{t("Protection")}<a href="#" className="hover:text-[#f34d01e6]">{t("PrivacyPolicy")}</a>{t("And")}<a href="#" className="hover:text-[#f34d01e6]">{t("Service")}</a></h3>
+                <h3 className="text-center text-xs cursor-pointer">{t("Protection")} <a href="#" className="hover:text-[#f34d01e6]">{t("PrivacyPolicy")}</a>{t("And")}<a href="#" className="hover:text-[#f34d01e6]">{t("Service")}</a></h3>
             </div>
         </>
     );
